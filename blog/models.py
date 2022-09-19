48843ade9ec1d962f5cf570ed2db01ade95c5804
sanelighamari <sanelighamari@gmail.com>
2022-09-19 13:00:28 +0430

    database changes
    I made some changes for my project's sql

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,15 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):    
+    #image
+    #author
     title = models.CharField(max_length=200)
     content = models.TextField()
+    #tag
+    #category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
